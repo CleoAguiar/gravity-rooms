c9f49527c06f0c50303f3eeace4a943a97023aee
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-06 11:18:29 -0300

    feat: configured layer, mask and door with level end

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,4 +1,4 @@
-extends StaticBody2D
+extends Area2D
 
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
@@ -7,3 +7,6 @@ func open_door():
 	print("abrindo porta")
 	sprite_2d.play("opening")
 	collision.set_deferred("disabled", true)
+
+func _on_body_entered(body: Node2D) -> void:
+	print("Passou de fase")

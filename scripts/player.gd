fed77a578d812227dff33de2ce41c93c7c4892ed
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-06 13:27:23 -0300

    refactor: state machine for the player

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,12 +1,80 @@
 extends CharacterBody2D
 
+enum PlayerState {
+	idle, 
+	jump,
+	run
+}
+
+@onready var animated_sprite = $AnimatedSprite2D
 
 const SPEED = 200.0
 const JUMP_VELOCITY = -300.0
 
-@onready var animated_sprite = $AnimatedSprite2D
-
+var status : PlayerState
 var gravity_direction := 1 # 1 = normal, -1 = invertida
+
+func move():
+	# Movimento horizontal
+	var direction := Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+
+	if direction > 0:
+		animated_sprite.flip_h = false
+	elif direction < 0:
+		animated_sprite.flip_h = true
+
+func go_to_idle_state():
+	status = PlayerState.idle
+	animated_sprite.play("idle")
+func idle_state():
+	move()
+	if velocity.x !=0:
+		go_to_run_state()
+		return
+
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func exit_to_idle_state():
+	pass
+	
+func go_to_jump_state():
+	status = PlayerState.jump
+	animated_sprite.play("jump")
+	velocity.y = JUMP_VELOCITY * gravity_direction
+func jump_state():
+	move()
+	if is_on_floor():
+		if velocity.x == 0:
+			go_to_idle_state()
+		else:
+			go_to_run_state()
+		return
+func exit_to_jump_state():
+	pass
+
+func go_to_run_state():
+	status = PlayerState.run
+	animated_sprite.play("run")
+func run_state():
+	move()
+	if velocity.x == 0:
+		go_to_idle_state()
+		return
+	if Input.is_action_just_pressed("jump"):
+		go_to_jump_state()
+		return
+
+func exit_to_run_state():
+	pass
+
+func _ready() -> void:
+	go_to_idle_state()
 
 func _physics_process(delta: float) -> void:
 	# Alternar gravidade
@@ -16,42 +84,23 @@ func _physics_process(delta: float) -> void:
 		# Atualiza o "chão"
 		up_direction = Vector2.UP * gravity_direction
 		
-		# Vira o sprite
+		# Vira o sprite gravidade
 		animated_sprite.flip_v = gravity_direction == -1
 		
 		# Impulso que dá o "feeling bom"
 		# 150 → mais suave / 200 → equilíbrio / 250+ → mais arcade / agressivo
 		velocity.y = 200 * gravity_direction
-
+		
 	# Aplicar gravidade
 	if not is_on_floor():
 		velocity += get_gravity() * gravity_direction * delta
-
-	# Pulo
-	if Input.is_action_just_pressed("jump") and is_on_floor():
-		velocity.y = JUMP_VELOCITY * gravity_direction
-
-	# Movimento horizontal
-	var direction := Input.get_axis("move_left", "move_right")
+		
+	match status:
+		PlayerState.idle:
+			idle_state()
+		PlayerState.jump:
+			jump_state()
+		PlayerState.run:
+			run_state()
 	
-	if direction > 0:
-		animated_sprite.flip_h = false
-	elif direction < 0:
-		animated_sprite.flip_h = true
-	
-	# Animações
-	if is_on_floor():
-		if direction == 0:
-			animated_sprite.play("idle")
-		else:
-			animated_sprite.play("run")
-	else:
-		animated_sprite.play("jump")
-	
-	# Movimento
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-
 	move_and_slide()

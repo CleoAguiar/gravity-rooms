3f82125480bba19f085acec3df3fa0c37d9a77bf
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-06 11:33:54 -0300

    feat: door system, open only with a key

--- scripts/door.gd
+++ scripts/door.gd
@@ -3,10 +3,15 @@ extends Area2D
 @onready var sprite_2d: AnimatedSprite2D = $Sprite2D
 @onready var collision: CollisionShape2D = $CollisionShape2D
 
+var key_collected = false
+
 func open_door():
 	print("abrindo porta")
 	sprite_2d.play("opening")
-	collision.set_deferred("disabled", true)
+	key_collected = true
 
-func _on_body_entered(body: Node2D) -> void:
-	print("Passou de fase")
+func _on_body_entered(_body: Node2D) -> void:
+	if key_collected:
+		print("Passou de fase")
+	else:
+		print("Sem chave")
--- scripts/key.gd
+++ scripts/key.gd
@@ -3,6 +3,6 @@ extends Area2D
 signal collected
 
 func _on_body_entered(_body: Node2D) -> void:
-	print("I keep the key")
+	print("Pegou a chave")
 	emit_signal("collected")
 	queue_free()

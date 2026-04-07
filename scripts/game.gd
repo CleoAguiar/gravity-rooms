a6ae0f0965184084005f53b90a695a2313acdbfe
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-07 16:01:59 -0300

    feat: reload current scene

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+func reset_level():
+	var tween = create_tween()
+	tween.tween_property(self, "modulate:a", 0.0, 0.15)
+	await tween.finished
+	get_tree().reload_current_scene()
+
+func _process(_delta):
+	if Input.is_action_just_pressed("reset"):
+		reset_level()

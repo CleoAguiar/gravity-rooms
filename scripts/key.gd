31c75e0360765ad9a73e73bcd5855edb0aef5303
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-08 17:39:10 -0300

    feat: Key sound

--- scripts/key.gd
+++ scripts/key.gd
@@ -2,7 +2,23 @@ extends Area2D
 
 signal collected
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var collision: CollisionShape2D = $CollisionShape2D
+@onready var sound: AudioStreamPlayer2D = $PickupSound
+
+
+
+
 func _on_body_entered(_body: Node2D) -> void:
-	print("Pegou a chave")
+	sound.pitch_scale = randf_range(0.95, 1.1)
+	sound.play()
+	
+	# Esconde visualmente
+	sprite.visible = false
+	collision.disabled = true
+	
+	# Espera o som terminar antes de deletar
+	await sound.finished
+	
 	emit_signal("collected")
 	queue_free()

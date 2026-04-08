2b54a3761e358e96fa842f85d61aa872fb0604af
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-08 20:15:50 -0300

    feat: ambient sound

--- scripts/game.gd
+++ scripts/game.gd
@@ -1,10 +1,19 @@
 extends Node2D
 
+@onready var ambient_sound: AudioStreamPlayer2D = $AmbientSound
+
 func reset_level():
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 0.0, 0.15)
 	await tween.finished
 	get_tree().reload_current_scene()
+
+func _ready():
+	ambient_sound.volume_db = -5
+	ambient_sound.play()
+	
+	var tween = create_tween()
+	tween.tween_property(ambient_sound, "volume_db", -15, 2.0)
 
 func _process(_delta):
 	if Input.is_action_just_pressed("reset"):

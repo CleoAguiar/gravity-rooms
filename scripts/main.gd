34e646ea184b16c77d84e77a9e3d42784ba1b44d
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-10 12:43:11 -0300

    fix: add instruction tiles

--- scripts/main.gd
+++ scripts/main.gd
@@ -6,10 +6,16 @@ extends Node2D
 @onready var ambient_sound: AudioStreamPlayer = $Audio/Ambient/AmbientSound
 
 const START_LEVEL = "res://scenes/levels/level_01_neon_lab.tscn"
+
 var current_level_path := ""
 var current_level: Node = null
+var loading := false
+
 
 func reset_level():
+	if loading:
+		return
+
 	var tween = create_tween()
 	tween.tween_property(fade, "self_modulate:a", 1.0, 0.15)
 	await tween.finished
@@ -23,45 +29,56 @@ func reset_level():
 
 	load_level(current_level_path)
 
+
 func setup_level(level):
+	print("setup level")
 	tutorial_manager.instruction = level.get_instructions()
 	tutorial_manager.setup_instructions()
 
+
 func load_level(path):
+	if loading:
+		return
+
+	loading = true
 	current_level_path = path
 
+	# limpa level atual corretamente
 	for child in level_container.get_children():
-		child.queue_free()
+		child.free()
 
+	# instancia novo level
 	var level = load(path).instantiate()
-	level_container.add_child(level)
 
+	level_container.add_child(level)
 	current_level = level
 
+	# garante que tudo está pronto antes de setup
 	await get_tree().process_frame
 
-	setup_level(level)
+	call_deferred("setup_level", level)
 
-	await get_tree().create_timer(0.1).timeout
-
+	# fade in
 	var tween = create_tween()
 	tween.tween_property(fade, "self_modulate:a", 0.0, 0.25)
 	await tween.finished
 
+	loading = false
+
+
 func _ready():
-	fade.self_modulate.a
-	
 	load_level(START_LEVEL)
-	
-	# fade in inicial
+
+	# fade inicial
 	var tween = create_tween()
 	tween.tween_property(fade, "modulate:a", 0.0, 0.3)
-	
+
 	ambient_sound.volume_db = -5
 	ambient_sound.play()
-	
+
 	var tween_audio = create_tween()
 	tween_audio.tween_property(ambient_sound, "volume_db", -15, 2.0)
+
 
 func _process(_delta):
 	if Input.is_action_just_pressed("reset"):

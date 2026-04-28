a1d926a2921aabf387def972068bb9db34f720e9
Cleo Aguiar <cleotaisas@gmail.com>
2026-04-28 20:27:55 -0300

    refact: remove level_end and organize scenes folder

--- scripts/credits.gd
+++ scripts/credits.gd
@@ -11,5 +11,4 @@ func _on_timer_timeout():
 	go_to_main_menu()
 
 func go_to_main_menu():
-	get_tree().change_scene_to_file("res://scenes/levels/level_end.tscn")
-	#get_tree().change_scene_to_file("res://scenes/MainMenu.tscn")
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
